c1d3688196db9d78487175ddb0a6adcdff4f8bb9
Mac <zombietux@macui-macbook-pro-2.local>
2019-03-28 22:49:50 +0900

    Initial Commit

---
+++ RememberMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RememberMe
+//
+//  Created by zombietux on 28/03/2019.
+//  Copyright © 2019 zombietux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
