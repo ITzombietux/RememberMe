a4d29c09465293dbbaac5058271b5f7d99756c31
zombietux <zombietux@Macui-MacBook-Pro-2.local>
2019-03-29 22:59:54 +0900

    How Data Sources Interact with View Controllers and Other Objects

--- RememberMe/TopFriendsViewController.swift
+++ RememberMe/TopFriendsViewController.swift
@@ -11,5 +11,27 @@ import UIKit
 class TopQuestionsViewController: UIViewController, Stateful {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var friendsDataSource: FriendDataSource?
     var stateController: StateController?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        guard let topFriends = stateController?.topFriends else {
+            return
+        }
+        friendsDataSource = FriendDataSource(friends: topFriends)
+        tableView.dataSource = friendsDataSource
+        tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let friendViewController = segue.destination as? FriendViewController else {
+            return
+        }
+        passState(to: friendViewController)
+        if let indexPath = tableView.indexPathForSelectedRow {
+            friendViewController.friend = friendsDataSource?.friend(at: indexPath)
+        }
+    }
+
 }
